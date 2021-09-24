cd43b136b74cafa62d29fe9a74ba58463c3c30bb
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-24 15:18:40 +0300

    Create C-x.py

---
+++ C-x.py
@@ -0,0 +1,77 @@
+import time
+
+
+t1 = time.time()
+
+x = "chaos"
+x1 = "Passion"
+x2 = "Moore's Law"
+x3 = "Launch Love"
+x4 = "must reduce carbonhydrate"
+x5 = "heart disease"
+x6 = "Iron Maiden"
+x7 = "Dream theater"
+x8 = "Mario vs Warrio"
+x9 = "Thirsty?"
+x10 = "chaos"
+x11 = "Herr's philosophical theory"
+x12 = "Yet there might be a point"
+x13 = "Crazy horses song"
+x14 = "a.i. Dini"
+x15 = "a.i. Dini"
+x16 = "a.i. did it"
+x17 = "WHO t π"
+χ18 = "WHO t 0"
+x19 = "WHO t -"
+x20 = "WHO t 0"
+x21 = "WHO t 5"
+x22 = "How t π"
+x23 = "How t 0"
+x24 = "How t 5"
+x25 = "How t 0"
+x26 = "5"
+x27 = "What do you mean"
+x28 = "future is frozen"
+x29 = "peacock = hot"
+x30 = "WHO = Egnarts"
+
+
+print(bool(x))
+print("fruits")
+print("Memory hole")
+print("Love lunch")
+print(bool(x4))
+print("Nasa")
+print("Be quick..")
+print("Deja vu")
+print("Prefer Warrio")
+print("an exercise?")
+print(bool(x10))
+print("used by different people..it's not usual")
+print(bool(x12))
+print("Mike Portnoy cover")
+print("Hu Dini")
+print("WHO Dini")
+print("WHO did it")
+print("WHO t π")
+print("WHO t 0")
+print("WHO t -")
+print("WHO t 0")
+print("WHO t 5")
+print("How t π")
+print("How t 0")
+print("How t 5")
+print("How t 0")
+print("What do you mean 5?")
+print("how much?..")
+print("peacock is not the future")
+print(bool(x29))
+print(bool(x30))
+
+
+
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
