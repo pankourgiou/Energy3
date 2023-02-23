2761abd37e7d07d64f68684f7bf722c7643c6bde
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-02-23 05:55:15 +0200

    Update and rename Function.py to Energy.R

---
+++ Energy.R
@@ -0,0 +1,2 @@
+myString <- "E = **2 = mcmod2 = gammamcmod2 = [(e**iπ)+2]mcmod2"
+print(myString)
--- Function.py
+++
@@ -1,6 +0,0 @@
-from gtts import gTTS
-import os
-text = "Sometimes to search is good, there are times you must believe and not search, in both cases you need to function"
-tts = gTTS(text)
-tts.save("hi.mp3")
-os.system("hi.mp3")
